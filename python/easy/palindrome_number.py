0f8237535e6f526a46ab0657a8e29a45cf7cdbfa
Alejandro Patiño García <alexpatino92@yahoo.com>
2025-07-31 14:41:53 +0200

    feat(): solved palindrome problem

---
+++ python/easy/palindrome_number.py
@@ -0,0 +1,14 @@
+#Given an integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+       
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return False
+    
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.isPalindrome(10))
+            
